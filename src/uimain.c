4988ef70c4bf5984b3fb47ab5a9b7e17e7e1972a
Christian <cavasquez1771@gmail.com>
2021-06-27 13:10:48 -0600

    updated ui. keep getting segmentation fault error when running program

--- src/uimain.c
+++ src/uimain.c
@@ -5,10 +5,58 @@
 
 int main() {
   printf("Welcome!\n");
-  char c[100];
-  
-  printf("\nPlease input a string:\n>");
-  fgets(c,100,stdin);
+  char c[5]; // user menu option
+  char sentence[100]; // user input string
+  List *history = init_history();
+  char hist_choice[100];
+  char *hist_out;
 
-  printf("the string is: %s\n",c);
+  while (1) {
+    printf("\nPlease select an option:\n");
+    printf("To input Tokens enter -> t\n");
+    printf("To view history enter -> h\n");
+    printf("To quit enter -> q\n>");
+    fgets(c,5,stdin);
+
+    int choice = menuOpt(c);
+
+    switch (choice) {
+
+    case 1:
+      printf("Please enter the string you would like tokeninze\n>");
+      fgets(sentence,100,stdin);
+      char **tokens = tokenize(sentence);
+      print_tokens(tokens);
+      add_history(history, sentence);
+      free_tokens(tokens);
+      break;
+    case 2:
+      printf("Displaying History... Enter a number to view a sentence from history ");
+      printf("or enter 'h' to view all history\n>");
+      fgets(hist_choice, 100, stdin);
+
+      if (hist_choice[0] == 'h') {
+	print_history(history);
+	break;
+      }
+      int id = atoi(hist_choice); // converts the string into an int
+      hist_out = get_history(history, id);
+      printf("%d: ", id);
+      if (*hist_out != '\0') {    // if hist exists
+	printf("%s\n", hist_out);
+	break;
+      }
+      printf("String not found. Enter a valid input\n"); // string not found
+      break;
+    case 0:
+      printf("Bye!");
+      goto done;
+    default:
+      printf("Unrecognized option, please try again.\n");
+    }
+  }
+
+  done:
+    return 0;
+
 }
